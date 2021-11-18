bd57bf584e346c949039b01d0021df3fd71446d2
mouradabidi345 <64220888+mouradabidi345@users.noreply.github.com>
2021-11-17 21:15:20 -0700

    Update Insight.py

--- Insight.py
+++ Insight.py
@@ -1,4 +1,4 @@
-#InisightAPI before automation the time
+
 import Levenshtein
 import pandas as pd
 import xlrd
@@ -23,7 +23,7 @@ import openpyxl as pxl
 
 
 #get the Agent base which Holds the Agents IncontctIDs and Email addresses:
-engine = sqlalchemy.create_engine("mssql+pyodbc://mabidi:ChangeMe2020$@ReportsWFM")
+engine = sqlalchemy.create_engine("mssql+pyodbc:")
 Agents = pd.read_sql_query('SELECT * FROM ASEA_REPORTS_STAGE.dbo.[Agent_Base]', engine)
 IncontactIDLis = Agents['InContactID'].tolist()
 
@@ -84,8 +84,8 @@ conn = http.client.HTTPSConnection("asea-global.bloomfire.com")
 
 # convert payload to json
 payload = json.dumps({
-  "api_key": "0d2168dd2079e8ef1e4e63fc13e8b53d801840ca",
-  "email": "ITdev@aseaglobal.com"
+  "api_key": "",
+  "email": ""
 })
 
 #Set headers
@@ -129,44 +129,7 @@ conn.close()
 #insert the string data into a dataframe
 StringData = StringIO(data)
 df = pd.read_csv(StringData, sep =",")
-#print(df)
-# print(type(df['Date']))
 
-#oldapi
-# Urlendpoint = "analytics/" + "member_engagement:full.csv?" + "date_range=" +  Monday_previous_week + "%20to%20" + Monday_same_weeks
-
-#            #get teh sessiontoken
-# conn = http.client.HTTPSConnection("asea-global.bloomfire.com")
-# payload = "{\r\n  \"email\": \"Mabidi@aseaglobal.com\",\r\n  \"password\": \"Ma@fawzia15\"\r\n}"
-# headers = {
-#   'Content-Type': 'application/json'
-# }
-# conn.request("POST", "/api/v2/login", payload, headers)
-# res = conn.getresponse()
-# data = res.read()
-# data = json.loads(data)
-# res.close()
-# conn.close()
-# session = data["session_token"]
-
-#            #Api to get the raw data
-# conn = http.client.HTTPSConnection("asea-global.bloomfire.com")
-# payload = ''
-# headers = {
-#   'Authorization': 'Bloomfire-Session-Token' +  session
-# }
-# conn.request("GET", "/api/v2/" + Urlendpoint, payload, headers)
-# res = conn.getresponse()
-# data = res.read()
-#               #conevrt raw data from bytes to string using .decode("utf-8")
-# data = data.decode("utf-8")
-# res.close()
-# conn.close()
-#            #insert the string raw data into a dataframe using StringIO()
-# StringData = StringIO(data)
-# df = pd.read_csv(StringData, sep =",")
-# dfcount = len(df.index)
-# print(dfcount)
           #convert the Date column back from String to Datetime type
 df['Date']= pd.to_datetime(df['Date'])
           #add a new column Date2 which has the format yyyy-m-d
@@ -199,18 +162,7 @@ TitlesEmailed = EmailedFilteredbyWeekStartSDate['actual_title']
 lists = TitlesEmailed.tolist()
 size = len(lists)
 
-# actual_title = []
-# similarity = []
-# for i in EmailedFilteredbyWeekStartSDate.Title:
-#     ratio = process.extract( i, df1.Title, limit=1)
-#     actual_title.append(ratio[0][0])
-#     similarity.append(ratio[0][1])
-# EmailedFilteredbyWeekStartSDate.loc[:,'actual_title'] = actual_title
-# EmailedFilteredbyWeekStartSDate.loc[:,'similarity'] = similarity
-# print(EmailedFilteredbyWeekStartSDate)
-# TitlesEmailed = EmailedFilteredbyWeekStartSDate['actual_title']
-# lists = TitlesEmailed.tolist()
-# size = len(lists)
+
 
 
 
@@ -333,9 +285,9 @@ ViewedArticles.to_sql(tableName, engine,
 
 
 
-engine = sqlalchemy.create_engine("mssql+pyodbc://mabidi:ChangeMe2020$@ReportsWFM")
-df1 = dataframe = pd.read_excel(r"C:\Users\mabidi\ASEA, LLC\Associate Support Operations - Insight/Insight Newsfeed Content.xlsx", sheet_name = 0, engine='openpyxl')
-engine = sqlalchemy.create_engine("mssql+pyodbc://mabidi:ChangeMe2020$@ReportsWFM")
+engine = sqlalchemy.create_engine("mssql+pyodbc:")
+df1 = dataframe = pd.read_excel(r"Insight Newsfeed Content.xlsx", sheet_name = 0, engine='openpyxl')
+engine = sqlalchemy.create_engine("mssql+pyodbc:")
 df0 = pd.read_sql_query('SELECT * FROM ASEA_REPORTS_STAGE.dbo.[Titles Emailed]', engine)
 df0.drop(df0.index, inplace=True)
 df0 = df0.append(df1)
